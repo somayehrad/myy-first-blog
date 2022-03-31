04e459ab1257f67594af89b2f66475e7d5728e0c
somayehrad <somayeh.navabirad@gmail.com>
2022-04-01 03:15:19 +0430

    phase 3

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.post_list,name='post_list'),
     path('date',views.view_date,name='view_date'),
     path('jm/',views.view_cv,name='jm'),
-    path('post_details/<int:pk>',views.post_details,name='post_detail')
+    path('post_details/<int:pk>',views.post_details,name='post_detail'),
+    path('post/new/',views.post_new,name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
